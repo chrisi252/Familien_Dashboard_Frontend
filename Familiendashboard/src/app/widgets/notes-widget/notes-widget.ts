85cdc40aafc41adc288a6af405339a47f67baa61
Thi Quynh Nhi Duong <duongtiq@schaeffler.com>
2026-02-23 18:47:11 +0100

    setup widgets

---
+++ Familiendashboard/src/app/widgets/notes-widget/notes-widget.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notes-widget',
+  imports: [],
+  templateUrl: './notes-widget.html',
+  styleUrl: './notes-widget.css',
+})
+export class NotesWidget {}
