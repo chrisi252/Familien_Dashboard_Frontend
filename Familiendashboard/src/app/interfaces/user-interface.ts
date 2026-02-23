58556f537e26f5333160c5cb6d2ad4a80c4a3acc
beckerni <nico.becker@heidelber.com>
2026-02-23 16:42:50 +0100

    add initial interfaces and Services

---
+++ Familiendashboard/src/app/interfaces/user-interface.ts
@@ -0,0 +1,3 @@
+export interface UserInterface {
+    //hier dann Datenmoddel wie im Backend machen
+}
---
+++ Familiendashboard/src/app/services/api-service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  //hier dann die ganzen daten aus dem Backend holen z.B. die  Benutzerr, die Termine, die ToDos usw.
+}
